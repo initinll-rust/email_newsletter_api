b105947ed1dfb5772a8554718c3097c7b18b803a
Nitin Londhe <nitin.londhe@genmills.com>
2022-12-29 01:28:45 +0530

    added sqlx script in bat file

--- scripts/postgresdb.bat
+++ scripts/postgresdb.bat
@@ -1,14 +1,23 @@
 @echo off
 
-echo use startdb to start postgres db and stopdb to stop postgres db
+echo "CMD HELP ::"
+echo "CMD: startdb -> starts postgres db"
+echo "CMD: stopdb -> stops postgres db"
+echo "CMD: isqlx -> installs sqlx binary for postgres db"
+
 
 if %1==startdb (
 	docker container rm postgresql
 	docker pull postgres
-	docker run -d --name postgresql -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=newsletter -p 5432:5432 postgres postgres-N 1000
+	docker run -d --name postgresql -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=newsletter -p 5432:5432 postgres postgres-N 1000	
 )
 
 if %1==stopdb (
 	docker stop postgresql
 	docker container rm postgresql
+)
+
+if %1==isqlx (
+	cargo install sqlx-cli --no-default-features --features native-tls,postgres
+	set DATABASE_URL=postgres://postgres:password@localhost:5432/newsletter
 )
