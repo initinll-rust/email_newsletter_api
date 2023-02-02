0c524c92a4f927ebc6a8fdc1cc14de6fda8c6409
Nitin Londhe <nitin.londhe@genmills.com>
2023-02-02 23:36:44 +0530

    added telemetry using tracing crate

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
 use std::net::TcpListener;
 use sqlx::PgPool;
-use env_logger::Env;
 
 use email_newsletter_api::startup::run_app;
 use email_newsletter_api::configuration::get_configuration;
+use email_newsletter_api::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<()> {           
 
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-            .await
-            .expect("Failed to connect to Postgres.");
+        let subscriber = get_subscriber("zero2prod".into(),"info".into());
+        init_subscriber(subscriber);
 
-    let host = configuration.database.host;
-    let port = configuration.application_port;
-    let address = format!("{}:{}",host, port);
+        let configuration = get_configuration().expect("Failed to read configuration.");
+        let connection_pool = PgPool::connect(&configuration.database.connection_string())
+                .await
+                .expect("Failed to connect to Postgres.");
 
-    let listener = TcpListener::bind(&address)
-            .expect("Failed to bind random port");
-    println!("server listening at - {}", &address);
-    
-    run_app(listener, connection_pool)?.await
+        let host = configuration.database.host;
+        let port = configuration.application_port;
+        let address = format!("{}:{}",host, port);
+
+        let listener = TcpListener::bind(&address)
+                .expect("Failed to bind random port");
+        println!("server listening at - {}", &address);
+
+        run_app(listener, connection_pool)?.await
 }
--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,5 +1,7 @@
 use actix_web::HttpResponse;
 
 pub async fn health_check() -> HttpResponse {
+    // logging
+    tracing::info!("checking app health");
     HttpResponse::Ok().finish()
 }
