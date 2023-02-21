9dc9752b462001b46584bf9af252ed95150bec88
Nitin Londhe <nitin.londhe@genmills.com>
2023-02-21 23:16:25 +0530

    added sqlx offline mode

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,10 @@ async fn main() -> std::io::Result<()> {
         init_subscriber(subscriber);
 
         let configuration = get_configuration().expect("Failed to read configuration.");
-        let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-                .await
-                .expect("Failed to connect to Postgres.");
+        
+        let connection_pool = PgPool::connect_lazy(
+                &configuration.database.connection_string().expose_secret()
+        ).expect("Failed to connect to Postgres.");
 
         let host = configuration.database.host;
         let port = configuration.application_port;
